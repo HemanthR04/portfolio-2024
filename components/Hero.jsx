74120f52868938e409bcb132e3dd8b7db0987371
hr3003 <hr3003@att.com>
2024-04-13 20:02:33 +0530

    fixed background color issue

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='hero py-[100px] w-full text-white flex items-center justify-center'>
+        <div className='hero py-[80px] w-full text-white flex items-center justify-center'>
            <div className='hero-box bg-secondary w-[80%] h-[551px] rounded-[24px] flex flex-col items-center justify-center mx-auto'>
                 <h1 className='text-primary font-bold text-[36px] leading-[40px] w-[90%] mx-auto '>HI THERE,<br/>I’M HEMANTH.</h1>
                 <p className='text-primary mt-[20px] w-[90%] leading-[20px]'>a frontend developer and designer bringing digital ideas into reality</p>
